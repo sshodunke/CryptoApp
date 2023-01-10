3410ec03d916060f67c3b4781feb5ba80fb1fe3a
Smith Shodunke <sshodunke1@gmail.com>
2023-01-10 19:54:54 +0000

    Added CoinRepository DI

--- app/src/main/java/com/smithshodunke/cryptoapp/di/AppModule.kt
+++ app/src/main/java/com/smithshodunke/cryptoapp/di/AppModule.kt
@@ -1,9 +1,23 @@
 package com.smithshodunke.cryptoapp.di
 
+import com.smithshodunke.cryptoapp.data.remote.CoinpaprikaApi
+import com.smithshodunke.cryptoapp.data.repository.CoinRepositoryImpl
+import com.smithshodunke.cryptoapp.domain.repository.CoinRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinpaprikaApi) : CoinRepository {
+        return CoinRepositoryImpl(
+            api = api
+        )
+    }
+}
